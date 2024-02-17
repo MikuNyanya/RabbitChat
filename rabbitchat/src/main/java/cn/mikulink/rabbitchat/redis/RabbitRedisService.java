daa4a15d19d965d66257497ee5eff8396f4161dd
MikuNyanya <k19831221@163.com>
2024-02-18 02:21:08 +0800

    设置redis key过期时间

--- rabbitchat/src/main/java/cn/mikulink/rabbitchat/redis/RabbitRedisService.java
+++ rabbitchat/src/main/java/cn/mikulink/rabbitchat/redis/RabbitRedisService.java
@@ -1,8 +1,12 @@
 package cn.mikulink.rabbitchat.redis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * MikuLink created in 2024/2/18 1:24
@@ -14,6 +18,9 @@ import org.springframework.stereotype.Service;
 public class RabbitRedisService {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+    //redis默认过期时间
+    @Value("${rabbitchat.redisexpiretime}")
+    private Long expireTime;
 
     /**
      * 写入redis
@@ -22,7 +29,18 @@ public class RabbitRedisService {
      * @param value 写入内容
      */
     public void set(String key, String value) {
+        set(key,value,expireTime,TimeUnit.SECONDS);
+    }
+
+    /**
+     *  写入redis
+     * @param key redis key
+     * @param value 写入内容
+     * @param expireTime 过期时间
+     */
+    public void set(String key, String value,Long expireTime,TimeUnit timeUnit) {
         stringRedisTemplate.opsForValue().set(key, value);
+        stringRedisTemplate.expire(key,expireTime,timeUnit);
     }
 
     /**
